9b679158bbc46af62657994c2f23e3260743311f
xiangwenshi <1656903534@qq.com>
2022-04-21 22:53:42 +0800

    kt学习记录相关

---
+++ app/src/main/java/com/je/xwsgame/VibratorDemoActivity.java
@@ -0,0 +1,47 @@
+package com.je.xwsgame;
+
+import android.app.Activity;
+
+import android.content.Context;
+
+import android.os.Bundle;
+
+import android.os.Vibrator;
+
+public class VibratorDemoActivity extends Activity {
+
+    private Vibrator vibrator;
+
+    /** Called when the activity is first created. */
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_vibrator_demo);
+
+        /*
+
+         * 想设置震动大小可以通过改变pattern来设定，如果开启时间太短，震动效果可能感觉不到
+
+         * */
+
+        vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+
+        long [] pattern = {100,400,100,400}; // 停止 开启 停止 开启
+
+        vibrator.vibrate(pattern,2); //重复两次上面的pattern 如果只想震动一次，index设为-1
+
+    }
+
+    public void onStop(){
+
+        super.onStop();
+
+        vibrator.cancel();
+
+    }
+
+}
---
+++ app/src/main/java/com/je/xwsgame/service/MyService2.java
@@ -0,0 +1,66 @@
+package com.je.xwsgame.service;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.je.xwsgame.MainActivity;
+
+public class MyService2 extends Service {
+    private static final String TAG = "MyService2";
+    int count;
+    boolean startCommanding = true;
+    public MyService2() {
+    }
+
+    @Override
+    public void onCreate() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Notification notification = new Notification.Builder(getApplicationContext(),"message").build();
+            startForeground(1,notification);
+        }
+        Log.i(TAG,"onCreate - Thread ID = ");
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent,int flags,int index){
+        count = 0;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (startCommanding){
+                    try {
+                        Thread.sleep(1000);
+                        count++;
+                        Log.d(TAG, "count====== : "+count);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }).start();
+        return super.onStartCommand(intent,flags,index);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onDestroy(){
+        startCommanding = false;
+        Log.d(TAG,"onDestroy - Thread ID = ");
+        super.onDestroy();
+    }
+}
