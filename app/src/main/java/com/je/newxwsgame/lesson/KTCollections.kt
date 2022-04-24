b0445ebb141851989f76c0a71a3b034b5214cb8f
xiangwenshi <1656903534@qq.com>
2022-04-24 10:41:45 +0800

    修改包名测试

---
+++ app/src/main/java/com/je/newxwsgame/lesson/KTCollections.kt
@@ -0,0 +1,73 @@
+package com.je.newxwsgame.lesson
+
+fun main() {
+    //kotlin数组
+    //数组里面的值可以是任意类型
+    val arrayNumber:Array<Any> = arrayOf(1,true,"ss")
+    val test = arrayNumber.get(1)
+
+    var arrayNumber1:Array<Int> = arrayOf(2,1)
+    var arrayIfNull:Array<String?> = arrayOfNulls<String>(5)
+    arrayIfNull[1] = "ss"
+    val asc = Array(5){i:Int ->(i*i).toString()}
+    val asc1 = ByteArray(5)
+
+    for (item in arrayNumber){
+        println(item.toString())
+    }
+    for (i in arrayNumber.indices){
+        println(arrayNumber[i].toString())
+    }
+    arrayNumber.forEach {
+        println(it.toString())
+    }
+
+    val array = mutableListOf<String>()
+    array.add("ss")
+    array.count()
+    val array1 = listOf<String>()
+
+    val map = mutableMapOf<Int,String>()
+    map[2] = "ss"
+
+    val map1 = mapOf<String,String>(Pair("key","value"), Pair("22","ff"))
+    println(map1["22"])
+
+    //跟list区别 不可以有相同的元素
+    val set = mutableSetOf<Int>()
+    set.add(1)
+    val set1 = setOf<Int>(1,2,2)
+    for (item in set1){
+        println("set${item.toString()}")
+    }
+
+    val test4:String = learn(11,22,testFun = {Persion("xiangwenshi").test()}).toString()
+    println("打印方法返回值${test4}")
+    Persion("项文师").test()
+    Persion("项文师").test()
+    Persion1.test()
+}
+
+fun learn(a:Int,b:Int,testFun:() -> String):Boolean{
+    return a > b
+}
+
+//普通类
+class Persion constructor(name:String){
+    var myname:String = ""
+    init {
+        myname = name;
+    }
+    fun test():String{
+        println("调用类里面的方法${myname}")
+        return "faff"
+    }
+}
+
+
+//静态类
+object Persion1{
+    fun test(){
+        println("调用类里面的方法")
+    }
+}
---
+++ app/src/main/java/com/je/newxwsgame/lesson/KTDataType.kt
@@ -0,0 +1,40 @@
+package com.je.newxwsgame.lesson
+
+//kotlin数据类型
+fun main() {
+
+    /**
+     * 什么是位宽（bit） 计算机中最小数据存储单位
+     * 1.在计算机当中 所有数字在存储的时候 都是二进制存储
+     * 二进制八位称为1个字节
+     * int数据类型占了4个字节 4*8 = 32 long 占了8个字节
+     *
+     */
+    val number = 100//编辑器会根据赋值，推断数据类型 这个number 是int 以为没超过int的最大值
+    val bigNumber = 80000000 //long类型
+    val longNUmber = 20L
+    val intNumber:Int = 1
+    val byteNumber:Byte = 1
+
+    //浮点类型
+    val doubleNumber = 3.1412334//double 32
+    val foubleNumber = 3.1412334f//float 64
+
+    println("foubleNumber: = "+doubleNumber);
+
+    //字符类型声明
+    var char:Char = '0'
+    var istest:Boolean = false
+    var istest1 = false
+    var str = "1211111111"
+    println("str[1]: = "+str[1].toString());
+    println("the result is $str")
+    println("the result length ${str.length}")
+    val helloworld4 = """
+        
+    """.trimIndent()
+
+    val numberInt:Int = 3/2  //输出1
+
+    val numberDouble = 3/2.toDouble()//输出1.5
+}
